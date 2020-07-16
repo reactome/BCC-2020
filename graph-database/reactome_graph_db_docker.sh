d116885e3dac79fd20ea132c4fbd4d6c0a175cfb
Joel Weiser <joel.weiser@oicr.on.ca>
2020-07-16 17:00:31 -0400

    Update reactome_graph_db_docker.sh

--- graph-database/reactome_graph_db_docker.sh
+++ graph-database/reactome_graph_db_docker.sh
@@ -10,6 +10,6 @@ mkdir -p "$target_database_dir"
 
 tar -zxvf reactome.graphdb.tgz
 
-mv reactome.graphdb "$target_database_dir/graph.db"
+mv graph.db "$target_database_dir/graph.db"
 
 docker run --name reactome_graph_db -p 7687:7687 -p 7474:7474 -e NEO4J_dbms_allow__upgrade=true -e NEO4J_AUTH=none -v "$target_data_dir:/data" neo4j:3.5.19
